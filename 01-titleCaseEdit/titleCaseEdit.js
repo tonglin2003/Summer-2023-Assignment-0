266cb928c7df1698537312a0b2cbdfb26d34b4ef
tonglin <tl82003@gmail.com>
2023-06-03 16:38:38 -0400

    completed task 00 and 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,14 @@
 function titleCaseEdit(title) {
   // Insert code here;
+
+
+  let string_arr = title.split(' ');
+
+  let capitalizedTitle = string_arr.map(word => word[0].toUpperCase() + word.slice(1));
+  let result = capitalizedTitle.join(' ');
+  
+  return result;
+
 }
 
 // Do not edit this line;
