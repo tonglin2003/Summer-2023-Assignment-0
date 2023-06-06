00b04d08d5341af631e6987f196802fe18cb1b7b
tonglin <tl82003@gmail.com>
2023-06-06 17:07:00 -0400

    more questions completed

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,33 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  if (nums.length <= 1) return nums[0];
+  
+
+  let min = nums[0], max = nums[0];
+
+  [10, 3, 5, 1, 4, 8];
+
+  for (let i = 1; i < nums.length; i++)
+  {
+    if (nums[i] < min)
+    {
+      min = nums[i];
+    } 
+    else if (nums[i] > max)
+    {
+      max = nums[i];
+    }
+
+  }
+  
+  return min+max;
+
 }
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
+
+// brutal approach: find the min and max in one loop, O(n)
+
+// possible way: sort the array first, then take the first and last element
+// but fastest sorting algorithm is quicksort with o(n log n), and didn't improve the time complexity
--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,40 @@
+function reverseStr(word)
+{
+  let reversedArr = word.split('');
+
+  let start = 0;
+  let end = reversedArr.length - 1;
+
+  while (start < end)
+  {
+    let tmp = reversedArr[start];
+    reversedArr[start] = reversedArr[end];
+    reversedArr[end] = tmp;
+
+    start++;
+    end--;
+  }
+
+  let reversed = reversedArr.join('');
+  return reversed;
+
+  
+}
 function isPalindrome(word) {
   // Insert code here;
+
+  const reversedWord = reverseStr(word);
+  return word === reversedWord;
+
 }
 
 // Do not edit this line;
 module.exports = isPalindrome;
+
+// brutal approach: use two pointers in a for loop, one pointer points to the first letter
+// another pointer points to the last letter
+// in the loop, compare if the two pointers' value is the same
+// time complexity: o(n), worst case: o(n/2)
+
+// another way: reverse the string, and see if it's the same with the original word
+// time complexity: o(n), worst case: o(n/2)
