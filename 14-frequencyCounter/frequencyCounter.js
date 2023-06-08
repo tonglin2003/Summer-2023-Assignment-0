20b0cf400319a7c14db4cde345d3b0f8b3d1e48d
tonglin <tl82003@gmail.com>
2023-06-08 11:56:13 -0400

    more questions completed

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,20 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let frequencyObject = {};
+  const wordArr = word.split('');
+
+  wordArr.forEach(element => {
+    if (frequencyObject[element])
+    {
+      frequencyObject[element]++;
+    }
+    else
+    {
+      frequencyObject[element] = 1;
+    }
+  });
+
+  return frequencyObject;
 }
 
 // Do not edit this line;
--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,13 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  let product = args[0];
+  for (let i = 1; i < args.length; i++)
+  {
+    product *= args[i];
+  }
+
+  return product;
+
 }
 
 // Do not edit this line;
